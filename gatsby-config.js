12f04ccaf537d2583a24006e7345e28a4b230611
gaurangpotdar <gaurangpotdar@gmail.com>
2021-08-13 00:15:57 +0530

    added messages and config

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+const siteMetadata =  {
+      title: `The Localhost Blog`,
+      description: `This is my coding blog where I write about my coding journey.`,    
+}
+
+module.exports = {
+    plugins: [
+      {
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          extensions: [`.mdx`, `.md`],
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/posts`,
+          name: `posts`,
+        },
+      },
+    ],
+  };
